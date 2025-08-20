46aa6884c8001372129c99bdfc7aaaffb8ad581a
Sortjakke <erick.matheusrs1003@gmail.com>
2025-08-20 15:41:18 -0300

    docs: addition of README and final links

---
+++ README.md
@@ -0,0 +1,29 @@
+# Frontend Mentor - Results summary component solution
+
+![Design preview for the Results summary component coding challenge](./preview.jpg)
+
+This is a solution to the [Results summary component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Links](#links)
+- [Built with](#built-with)
+- [Author](#author)
+
+## Links
+
+- Repository URL: [https://github.com/SortJakke/FM-results_summary]
+- Live Site URL: [https://fm-results-summary-delta.vercel.app/]
+
+## Built with
+
+- Vite
+- React
+- TypeScript
+- TailwindCss
+
+## Author
+
+- Author - [ SortJakke ]
+- Frontend Mentor - [https://www.frontendmentor.io/profile/SortJakke]
+- GitHub - [https://github.com/SortJakke]
--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,31 @@ function App() {
   const results = data.user as userData
 
   return (
-    <main className="font-[Hanken_Grotesk] min-h-screen bg-[hsl(221,100%,96%)] flex items-center justify-center md:p-4">
+    <main className="font-[Hanken_Grotesk] min-h-screen bg-[hsl(221,100%,96%)] flex flex-col items-center justify-center md:p-4">
       <div className="bg-white md:rounded-3xl shadow-lg grid md:grid-cols-2 w-full max-w-2xl overflow-hidden">
         <ScoreSection score={results.score} />
         <SummarySection data={results.categories} />
+      </div>
+      <div className="text-center my-4 mx-2 text-sm">
+        Challenge by{" "}
+        <a
+          className="text-[hsl(246,100%,66%)]"
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noopener"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          className="text-[hsl(246,100%,66%)]"
+          href="https://github.com/SortJakke"
+          target="_blank"
+          rel="noopener"
+        >
+          Sortjakke
+        </a>
+        .
       </div>
     </main>
   )
